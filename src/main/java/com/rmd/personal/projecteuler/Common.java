d8677e16dac1e35698e4360af2b93f4cfb3403e1
Rob Dimsdale <robert.dimsdale@softwire.com>
2013-08-29 20:57:25 -0600

    Reduce primes calculated by a factor of ten.

--- src/main/java/com/rmd/personal/projecteuler/Common.java
+++ src/main/java/com/rmd/personal/projecteuler/Common.java
@@ -1,12 +1,12 @@
 package com.rmd.personal.projecteuler;
 
-import java.util.Date;
 import java.util.LinkedList;
+import java.util.Date;
 import java.util.List;
 
 public final class Common {
 
-    private static final int MAX_PRIME_VALUE = 20000000;
+    private static final int MAX_PRIME_VALUE = 10000000;
 
     private Common() {
 
@@ -40,23 +40,6 @@ public final class Common {
         Date endTime = new Date();
 
         System.out.println("done - took " + (endTime.getTime() - startTime.getTime()) + " ms.");
-    }
-
-    protected static long sumPrimesUpTo(int end) {
-        boolean[] values = new boolean[end];
-
-        long sum = 0;
-
-        for (int i = 2; i < end; i++) {
-            if (!values[i]) {
-                primes.add((long) i);
-                sum += i;
-                for (int j = i; j < end; j += i) {
-                    values[j] = true;
-                }
-            }
-        }
-        return sum;
     }
 
     protected static long sum(long n) {
