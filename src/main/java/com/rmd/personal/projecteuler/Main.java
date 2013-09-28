7fa6b103824eda64bc952ba036544a3330340e8d
Rob Dimsdale <robert.dimsdale@oracle.com>
2013-09-27 20:45:18 -0600

    Problem 35.

--- src/main/java/com/rmd/personal/projecteuler/Main.java
+++ src/main/java/com/rmd/personal/projecteuler/Main.java
@@ -45,6 +45,7 @@ public final class Main {
         this.getProblemList().add(new P032());
         this.getProblemList().add(new P033());
         this.getProblemList().add(new P034());
+        this.getProblemList().add(new P035());
         this.getProblemList().add(new P037());
         this.getProblemList().add(new P038());
         this.getProblemList().add(new P039());
